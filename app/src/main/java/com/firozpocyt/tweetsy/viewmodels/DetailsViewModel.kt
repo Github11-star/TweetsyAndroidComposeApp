51bef42515e50d13b8c435b2811aa0c88a22a7c8
Firoz Shaikh <gitfiroz695@gmail.com>
2023-07-26 14:10:38 +0530

    Jetpack Compose - Viewmodels and Repository + MVVM

---
+++ app/src/main/java/com/firozpocyt/tweetsy/viewmodels/DetailsViewModel.kt
@@ -0,0 +1,24 @@
+package com.firozpocyt.tweetsy.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.firozpocyt.tweetsy.models.TweetListItem
+import com.firozpocyt.tweetsy.repository.TweetRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsViewModel @Inject constructor(private val repository: TweetRepository) : ViewModel() {
+
+    val tweets : StateFlow<List<TweetListItem>>
+        get() = repository.tweets
+
+    init {
+        viewModelScope.launch {
+        repository.getTweets("motivation")
+
+        }
+    }
+}
