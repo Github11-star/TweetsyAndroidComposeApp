e1f78fc8650f7aa233516a737c22d4d6ea12eeb8
Firoz Shaikh <gitfiroz695@gmail.com>
2023-08-07 12:00:56 +0530

    dependency update and AGP upgrade

--- app/src/main/java/com/firozpocyt/tweetsy/MainActivity.kt
+++ app/src/main/java/com/firozpocyt/tweetsy/MainActivity.kt
@@ -1,30 +1,18 @@
 package com.firozpocyt.tweetsy
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.firozpocyt.tweetsy.api.TweetsyAPI
 import com.firozpocyt.tweetsy.screens.CategoryScreen
 import com.firozpocyt.tweetsy.screens.DetailScreen
 import com.firozpocyt.tweetsy.ui.theme.TweetsyTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
